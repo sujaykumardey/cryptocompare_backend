0d87dd49f647fcc35d9853fef002155c60e3f47c
sujay dey <sujay.dey@mountblue.tech>
2020-05-12 22:01:21 +0530

    add login feature if user already exist reply already exist

--- models/login.js
+++ models/login.js
@@ -1,13 +1,27 @@
-const mongoose=require('mongoose');
-
+const mongoose = require('mongoose');
 
 const logindetail = new mongoose.Schema({
-    userName: String,
-    email: String,
-    date: { type: Date, default: Date.now },
-  });
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+});
 
+const Login = mongoose.model('login', logindetail);
 
-  const Login = mongoose.model('logins', logindetail);
-
-  module.exports=Login;
+module.exports = Login;
--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const router = require('express').Router();
 const Key = require('../models/key');
-const Login=require('../models/login')
-const Joi=require('joi')
+const Login = require('../models/login');
+const Joi = require('joi');
+const { validateUser } = require('../models/validate');
 
 router.get('/key', async (req, res) => {
   const result = await Key.find();
   res.send(result[0].key);
 });
 
-router.post('/auth', async (req, res) => {
-  const login = new Login({
-        userName: req.body.userName,
-        email: req.body.email,
-      });
-  const user = await login.save();
-  res.send(user)
 
+
+router.post('/users', async (req, res) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send('invalid user');
+  const useremail = await Login.findOne({ email: req.body.email });
+  if (useremail) return res.status(400).send('user already Register');
+
+  const login = new Login({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  const user = await login.save();
+  res.send(user);
 });
 
 
