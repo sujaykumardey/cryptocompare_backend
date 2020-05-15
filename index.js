10c1022411fb57105094c960ac3f355e816ac490
sujay dey <sujay.dey@mountblue.tech>
2020-05-15 12:10:31 +0530

    add header solve the core problem

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const winston=require('winston')
 
 
 app.use(express.json());
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.header('Access-Control-Allow-Credentials', true); 
+    next();
+})
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on ${port}...`));
