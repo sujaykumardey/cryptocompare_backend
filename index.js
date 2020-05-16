188210a86a7ee81a36b522994628570a3bc13e7b
sujay dey <sujay.dey@mountblue.tech>
2020-05-16 20:02:44 +0530

    add headers

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'x-requested-with,content-type'); // If needed
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-type,Accept,X-Access-Token,X-Key'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     next();
 })
