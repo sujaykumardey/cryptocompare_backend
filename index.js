17caca879d86de1596f127f55791b855bb890e21
sujay dey <sujay.dey@mountblue.tech>
2020-05-16 19:13:13 +0530

    change in header

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', '*'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'x-requested-with,content-type'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     next();
 })
