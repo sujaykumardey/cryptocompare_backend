a4d1631bffe1323025d77199a751b20ead6d09ba
sujay dey <sujay.dey@mountblue.tech>
2020-05-16 21:03:28 +0530

    add headers

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const bodyParser=require('body-parser')
 
 app.use(bodyParser.json());
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
-    res.header("Access-Control-Allow-Headers", "accept, x-requested-with, origin, content-type, cookie, pragma, cache-control, x-auth-token\r\n");
-    res.header('Access-Control-Allow-Credentials', true); 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
+    res.setHeader("Access-Control-Allow-Headers", "accept, x-requested-with, origin, content-type, cookie, pragma, cache-control, x-auth-token\r\n");
+    res.setHeader('Access-Control-Allow-Credentials', true); 
     next();
 })
 const port = process.env.PORT || 3000;
