bec1193f401a9e84852e9281164c06710e20745d
sujay dey <sujay.dey@mountblue.tech>
2020-05-17 12:18:50 +0530

    add body parser

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const bodyParser=require('body-parser')
 
 
 app.use(cors())
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on ${port}...`));
