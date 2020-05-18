ffbe194d5786b6a67a762ed2f2d021e9919f7dab
sujay dey <sujay.dey@mountblue.tech>
2020-05-18 18:01:52 +0530

    remove core module form api.js

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,4 @@
 const auth=require('../middleware/auth')
-const cors=require('cors')
 const express = require('express');
 const router = require('express').Router();
 const winston = require('winston');
